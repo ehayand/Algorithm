66c288f12459812d630c6d5376441a023561271e
Jaeyu Jo <ehay@naver.com>
2018-09-14 22:53:09 +0900

    Add 1
    add:#02490

---
+++ acmicpc/src/problem02490_윷놀이/Main.java
@@ -0,0 +1,45 @@
+package problem02490_윷놀이;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by ehay@naver.com on 2018-09-14
+ * Blog : http://ehay.tistory.com
+ * Github : http://github.com/ehayand
+ */
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] temp;
+        int sum, t = 3;
+
+        while (t-- > 0) {
+            sum = 0;
+            temp = br.readLine().split(" ");
+
+            for (int i = 0; i < 4; i++) sum += Integer.parseInt(temp[i]);
+
+            switch (sum) {
+                case 0:
+                    System.out.println("D");
+                    break;
+                case 1:
+                    System.out.println("C");
+                    break;
+                case 2:
+                    System.out.println("B");
+                    break;
+                case 3:
+                    System.out.println("A");
+                    break;
+                case 4:
+                    System.out.println("E");
+                    break;
+            }
+        }
+    }
+}
