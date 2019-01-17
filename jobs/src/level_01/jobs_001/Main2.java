a599b54e64692c502f8317141e1d716787f36a28
Jaeyu Jo <ehay@naver.com>
2019-01-17 13:24:11 +0900

    Add 1

---
+++ jobs/src/level_01/jobs_001/Main2.java
@@ -0,0 +1,13 @@
+package level_01.jobs_001;
+
+/**
+ * Created by ehay@naver.com on 2019-01-17
+ * Blog : http://ehay.tistory.com
+ * Github : http://github.com/ehayand
+ */
+
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("Hello Algorithm!");
+    }
+}
