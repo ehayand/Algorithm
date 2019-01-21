4a41b83b4842b26d7357f24bf1fa14435cb5e2b6
Jaeyu Jo <ehay@naver.com>
2019-01-21 14:27:01 +0900

    Add 1

---
+++ jobs/src/level_01/jobs_019/Main2.java
@@ -0,0 +1,31 @@
+package level_01.jobs_019;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Created by ehay@naver.com on 2019-01-21
+ * Blog : http://ehay.tistory.com
+ * Github : http://github.com/ehayand
+ */
+public class Main2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] temp = br.readLine().split(" ");
+        int n = Integer.parseInt(temp[0]);
+        int k = Integer.parseInt(temp[1]);
+        int result = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                k--;
+                if (k == 0) {
+                    result = i;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+}
