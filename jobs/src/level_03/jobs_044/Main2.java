e91c7990a0f3999dd6fbdc2fe335fbfa38cc94b9
Jaeyu Jo <ehay@naver.com>
2019-02-06 13:37:07 +0900

    Add 1

---
+++ jobs/src/level_03/jobs_044/Main2.java
@@ -0,0 +1,45 @@
+package level_03.jobs_044;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Created by ehay@naver.com on 2019-02-06
+ * Blog : http://ehay.tistory.com
+ * Github : http://github.com/ehayand
+ */
+
+public class Main2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        boolean[][] arr = new boolean[10][10];
+
+        String[] temp;
+
+        for (int i = 0; i < 10; i++) {
+            temp = br.readLine().split(" ");
+            for (int j = 0; j < 10; j++) {
+                if ("1".equals(temp[j])) arr[i][j] = !arr[i][j];
+            }
+        }
+
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < 10; i++) {
+                arr[k][i] = !arr[k][i];
+                arr[i][k] = !arr[i][k];
+            }
+            arr[k][k] = !arr[k][k];
+        }
+
+        for (boolean[] i : arr) {
+            for (boolean j : i)
+                sb.append(j ? 1 : 0).append(" ");
+            sb.replace(sb.length() - 1, sb.length(), "\n");
+        }
+
+        System.out.print(sb.toString());
+    }
+}
