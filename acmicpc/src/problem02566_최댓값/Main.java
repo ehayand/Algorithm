c6755cb7ac9b133a30e3705e44b7319de29e2e40
Jaeyu Jo <ehay@naver.com>
2019-02-07 15:14:14 +0900

    Add 1
    add:#02566

---
+++ acmicpc/src/problem02566_최댓값/Main.java
@@ -0,0 +1,40 @@
+package problem02566_최댓값;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Created by ehay@naver.com on 2019-02-07
+ * Blog : http://ehay.tistory.com
+ * Github : http://github.com/ehayand
+ */
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String[] temp;
+
+        int[][] arr = new int[9][9];
+
+        int row = 0;
+        int col = 0;
+        int max = 0;
+
+        for (int i = 0; i < 9; i++) {
+            temp = br.readLine().split(" ");
+            for (int j = 0; j < 9; j++) {
+                arr[i][j] = Integer.parseInt(temp[j]);
+                if (arr[i][j] > max) {
+                    max = arr[i][j];
+                    row = i;
+                    col = j;
+                }
+            }
+        }
+
+        sb.append(max).append("\n").append(++row).append(" ").append(++col);
+
+        System.out.println(sb.toString());
+    }
+}
